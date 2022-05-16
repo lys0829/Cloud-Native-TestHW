267f14e20b9f3c93cd05eac4dbe96eff8fdcff18
YU-HSIANG LIN <linlys@cs.nctu.edu.tw>
2022-05-16 19:52:40 +0800

    Complete testSimpleDiscounts

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 from tests.test import *
 
 testBasics()
+testSimpleDiscounts()
--- price.py
+++ price.py
@@ -1,2 +1,11 @@
 def price(books):
-    return len(books)*8.0
+    discount = [1.0, 1.0, 0.95, 0.9, 0.8, 0.75]
+    tmp = []
+    diff = 0
+    for b in books:
+        if b in tmp:
+            continue
+        diff += 1
+        tmp.append(b)
+
+    return 8.0 * diff * discount[diff] + 8.0 * (len(books)-diff)
